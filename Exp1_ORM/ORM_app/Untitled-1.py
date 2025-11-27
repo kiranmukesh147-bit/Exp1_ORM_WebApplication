7a74ef933808721322a215b3390945d1fe98bb7c
kiranmukesh147-bit <kiranmukesh147@gmail.com>
2025-11-27 11:52:07 +0530

    success

---
+++ Exp1_ORM/ORM_app/Untitled-1.py
@@ -0,0 +1,12 @@
+with open("sample.txt",'w') as f:
+    f.write("""iygwisygciy
+            idhsouchwuosdhvou
+            jdhcouhswdovh
+            jcjsijdip
+            jcoiswhjdcoiphjwesdpi
+            esuodchiouwsdhv""")
+with open("sample text",'r') as c:
+    t=c.read()
+i=t.split()
+k=len(i)
+print(i)
